2281a5e3b0268d033aacf43e07abefda662ac947
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2024-04-06 16:19:52 +0100

    Add Type FeedBack

--- src/main/java/com/mission/RHManager/Entites/FeedBack.java
+++ src/main/java/com/mission/RHManager/Entites/FeedBack.java
@@ -1,9 +1,7 @@
 package com.mission.RHManager.Entites;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.mission.RHManager.Entites.Enum.TypeFeedBack;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -21,5 +19,9 @@ public class FeedBack {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
      Long id;
      String feedBack;
+     @Enumerated(EnumType.STRING)
+     TypeFeedBack typeFeedBack;
+
+
 
 }
